3e9f71679a6ea93e8e63fa18649232991156314e
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-02-14 20:24:56 +0200

    task2_done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (image) => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData:"alt" });
